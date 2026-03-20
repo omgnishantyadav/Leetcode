fe8b9c589633cf66c75a6d557d8ea19e07afcace
Nishant Yadav <OMGNISHANTYADAV@GMAIL.COM>
2026-03-20 16:48:10 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (96.53%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(int i = 0; i<nums.size(); i++){
-            m[nums[i]]++;
+        //boore morey majority element
+        int n1,n2,m1,m2; 
+        n1 =0; n2 =0;
+        m1 = 0; m2 = 1;
+
+        for(auto m: nums){
+            if(m==m1) n1++;
+            else if(m==m2) n2++;
+            else if(n1==0){
+                m1=m; n1=1;
+            }
+            else if(n2==0){
+                m2=m; n2=1;
+            }
+            else {
+                n1--;
+                n2--;
+            }
         }
 
         vector<int> ans;
-        for(auto it: m){
-            if(it.second*3>nums.size()) ans.push_back(it.first);
-        }
+        if(3*count(nums.begin(), nums.end(), m1) > nums.size()) ans.push_back(m1);
+        if(3*count(nums.begin(), nums.end(), m2) > nums.size()) ans.push_back(m2);
+
         return ans;
     }
 };
