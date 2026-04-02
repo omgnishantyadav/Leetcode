035703a46673ba5772545cadc775245c928995b0
Nishant Yadav <OMGNISHANTYADAV@GMAIL.COM>
2026-04-02 16:14:23 +0530

    Time: 2244 ms (5.29%) | Memory: 457.6 MB (6.26%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    int solve(vector<vector<int>>& dp, string& t1, string t2, int i, int j){
+
+        if(i<=0||j<=0) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(t1[i-1]==t2[j-1]){
+            return dp[i][j] = 1+solve(dp,t1,t2,i-1,j-1);
+        }
+
+        return dp[i][j] = max(solve(dp,t1,t2,i-1,j), solve(dp,t1,t2,i,j-1));
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+
+        return solve(dp,text1,text2,n,m);
+        return dp[n][m];
+    }
+};
